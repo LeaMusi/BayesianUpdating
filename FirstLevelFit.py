bed849b0fb70d6c3763dd60697161239912eaba1
LeaMusi <clymene@web.de>
2019-08-22 18:59:21 +0200

    updated parameter recovery

--- FirstLevelFit.py
+++ FirstLevelFit.py
@@ -10,22 +10,27 @@ Created on %(date)s
 # optimizes the tau value for their Bayesian Updating iteratively using the bounded Brent-Dekker method, 
 # while fitting a simple linear regression at each step
 
-def FirstLevelFit(subj, iters, bounds, initial_simplex, simul):
+def FirstLevelFit(subj, simul):
     from scipy import optimize
     from LinearFit import LinearFit
     
-    def LinearFitForOpt(tau, subj, simul):
-        (costfun, lm, sigmasq) = LinearFit(tau, subj, simul)
-        return costfun
-    
-    def callbackF(x):
-        print(x)
+    ####### Fitting tau
+    #def LinearFitForOpt(tau, subj, simul):
+    #    (costfun, lm, sigmasq) = LinearFit(tau, subj, simul)
+    #    return costfun
+    #
+    #def callbackF(x):
+    #    print(x)
     
     #opt = optimize.minimize_scalar(fun=LinearFitForOpt, args=(subj, simul), bounds=bounds, method='Bounded', options={'maxiter':iters})
     #opt = optimize.minimize(fun=LinearFitForOpt, x0=1000, args=(subj, simul), method='Nelder-Mead', options={'maxiter':iters, 'initial_simplex':[[5],[5000]]}, callback=callbackF)
     #opt = optimize.basinhopping(func=LinearFitForOpt, x0=1000, full_output=True, minimizer_kwargs={'args':subj, simul}, niter=10, disp=True)
-    opttau = opt.x
+    #opttau = opt.x
     
-    (costfun, ml_lm, sigmasq) = LinearFit(opttau, subj, simul)
+    
+    
+    ####### Using predefined tau
+    tau = 
+    (costfun, ml_lm, sigmasq) = LinearFit(tau, subj, simul)
     
     return opttau, costfun, ml_lm
