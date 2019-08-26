9ecba11039176732eb1f96df5bb7a4b5a77e43ac
LeaMusi <clymene@web.de>
2019-08-26 15:52:19 +0200

    updated condition-based analysis

--- SemSur_modeling/trial_to_cond.py
+++ SemSur_modeling/trial_to_cond.py
@@ -9,42 +9,49 @@ Created on %(date)s
 import os
 import pandas as pd
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
 
 os.chdir("/Users/ringelblume/Desktop/GitHub/Semantic_Surprise_N400/SemSur_modeling/")
 
-
+# Open file for all participants' condition-wise mean amplitudes
 condmeans = pd.DataFrame(columns=['subject', 'condition', 'mean_amplitude'])
 
-
+############### Aggregating condition-wise mean amplitudes and performing t test across participants
 #subj=1
 for subj in range(1,41):
-
+    # Read in data
     if subj < 10:
         substr = '0' + str(subj)
     else:
-        substr = str(subj)        
-    
+        substr = str(subj)         
     sub_path = 'SemSurSequences/SemSurSequence_' + substr + '_tau=6.65.csv'
     dat = pd.read_csv(sub_path, encoding = 'unicode_escape', sep=";", index_col=0)
     
+    # Reshape data and aggregate, writing into common file for all participants and saving at each iteration
     dat = dat.rename(columns={"standard": "deviant"})
     dat['deviant'] = dat['deviant'].map({222: 1, 223: 1, 224:np.nan, 225:0})
-    
-    
     dat2 = dat.dropna(axis=0, subset=['deviant'], how="any")
     deviantmean = dat2['meanamp_ROI'][dat2['deviant'] == 1].mean()
     standardmean = dat2['meanamp_ROI'][dat2['deviant'] == 0].mean()
     condmeans.loc[(subj*2)-2,:] = [str(subj), 'deviant', deviantmean]
     condmeans.loc[(subj*2)-1,:] = [str(subj), 'standard', standardmean]
     condmeans.to_csv("/Users/ringelblume/Desktop/GitHub/Semantic_Surprise_N400/Condition_based_analysis/cond_meanamps.csv", sep=";")
-
     
+a = condmeans['mean_amplitude'][condmeans['condition'] == 'deviant']
+b = condmeans['mean_amplitude'][condmeans['condition'] == 'standard']
+ttest = scipy.stats.ttest_rel(a, b, axis=0)
+ttest = pd.DataFrame(ttest[0:2])
+ttest['parameter']=['T', 'p']
+ttest.to_csv("/Users/ringelblume/Desktop/GitHub/Semantic_Surprise_N400/Condition_based_analysis/ttest.csv", sep=";")
+    
+
 ################# Plots für eine Beispiel-VP:
-#sns.boxplot(x='deviant',y='baysur',data=dat)
-#plt.close()
+sns.boxplot(x='deviant',y='baysur',data=dat)
+plt.savefig("/Users/ringelblume/Desktop/GitHub/Semantic_Surprise_N400/Condition_based_analysis/boxplot_SemSur_by_condition.jpg", dpi=400)
+plt.close()
 
 
 # Deviant- und Surprisedaten
